910ad1030252e89d16806c883f4216a2b8c124c2
sebastien3k <121001998+sebastien3k@users.noreply.github.com>
2023-11-27 07:25:27 +0000

    2nd Commit: User Input Loop

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+while True:
+  user_input = input('> ')
+  if user_input.lower() == '':
+    pass
+  elif user_input.lower() in ['q', 'quit']:
+    break
+  else:
+    print("You've inputted: " + (user_input))
+  # print(f"That's dope {user_input}, anything else?")
